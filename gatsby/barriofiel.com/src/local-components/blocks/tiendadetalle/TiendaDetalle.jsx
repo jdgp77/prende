0dba5f284f5276d17bd2bac80b4755a623d4f626
Maria H <Santos P>
2020-04-30 23:52:36 -0500

    Se agregan estilos para el detalle final de las tiendas.

--- gatsby/barriofiel.com/src/local-components/blocks/tiendadetalle/TiendaDetalle.jsx
+++ gatsby/barriofiel.com/src/local-components/blocks/tiendadetalle/TiendaDetalle.jsx
@@ -4,6 +4,7 @@ import './TiendaDetalle.scss';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -44,10 +45,13 @@ export default function TiendaDetalle() {
           <Paper className={classes.paper}>
             <Grid container spacing={2}>
               <Grid item xs={12}>
+                <h1>Tienda de Don Pepe</h1>
                 <img className={classes.image} alt="complex" src="https://i.pinimg.com/originals/89/34/fe/8934fe9034e62c3f9ef4f02eea2c56ab.png" />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu tellus dictum, bibendum ante in, efficitur lectus. Quisque ac sem at arcu dictum hendrerit. Donec eget velit lectus. Fusce pellentesque luctus nulla. Proin imperdiet, turpis ac volutpat luctus, felis arcu congue diam, ut sollicitudin neque enim non purus. Curabitur vestibulum tristique dignissim. Nullam vitae molestie nibh, at dignissim enim. Sed faucibus ante ac turpis posuere viverra. Donec at metus nec augue rutrum imperdiet vitae sed diam.</p>
-                <p>Vestibulum dapibus ligula lectus, eget viverra erat pellentesque a. Nullam rutrum lacus sit amet ante tristique, eget interdum risus lobortis. Aliquam erat volutpat. Aliquam imperdiet vulputate dolor, eget molestie quam sodales a. In porttitor neque a dolor aliquet, nec blandit mauris consequat. Ut mauris ligula, accumsan ut nisi sed, faucibus pharetra tortor. Aenean ac venenatis purus, et vestibulum metus. Curabitur scelerisque posuere gravida. Praesent consectetur interdum justo, sit amet lobortis nulla feugiat eu. Aliquam tempus rhoncus odio. Sed blandit nulla in purus venenatis maximus. Curabitur molestie euismod felis, quis viverra enim accumsan vitae.</p>
-                <p>Curabitur et justo ante. Praesent in vehicula eros. Vestibulum eu arcu eget risus consequat tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur augue ac dui consectetur tincidunt. Suspendisse fringilla aliquet condimentum. Fusce commodo leo felis, sed imperdiet ex maximus non. Pellentesque pharetra nunc quis tellus vulputate, malesuada interdum quam viverra. Nulla ut nunc urna. In sollicitudin in lorem ac bibendum. Pellentesque faucibus imperdiet erat a posuere. Curabitur finibus nibh a dignissim sollicitudin. Aliquam euismod sodales nibh, ut tristique lectus vestibulum a. Duis sit amet interdum ipsum, in dapibus leo. Quisque sodales luctus cursus. Phasellus a convallis mauris.</p>
+                <Container >
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu tellus dictum, bibendum ante in, efficitur lectus. Quisque ac sem at arcu dictum hendrerit. Donec eget velit lectus. Fusce pellentesque luctus nulla. Proin imperdiet, turpis ac volutpat luctus, felis arcu congue diam, ut sollicitudin neque enim non purus. Curabitur vestibulum tristique dignissim. Nullam vitae molestie nibh, at dignissim enim. Sed faucibus ante ac turpis posuere viverra. Donec at metus nec augue rutrum imperdiet vitae sed diam.</p>
+                  <p>Vestibulum dapibus ligula lectus, eget viverra erat pellentesque a. Nullam rutrum lacus sit amet ante tristique, eget interdum risus lobortis. Aliquam erat volutpat. Aliquam imperdiet vulputate dolor, eget molestie quam sodales a. In porttitor neque a dolor aliquet, nec blandit mauris consequat. Ut mauris ligula, accumsan ut nisi sed, faucibus pharetra tortor. Aenean ac venenatis purus, et vestibulum metus. Curabitur scelerisque posuere gravida. Praesent consectetur interdum justo, sit amet lobortis nulla feugiat eu. Aliquam tempus rhoncus odio. Sed blandit nulla in purus venenatis maximus. Curabitur molestie euismod felis, quis viverra enim accumsan vitae.</p>
+                  <p>Curabitur et justo ante. Praesent in vehicula eros. Vestibulum eu arcu eget risus consequat tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus efficitur augue ac dui consectetur tincidunt. Suspendisse fringilla aliquet condimentum. Fusce commodo leo felis, sed imperdiet ex maximus non. Pellentesque pharetra nunc quis tellus vulputate, malesuada interdum quam viverra. Nulla ut nunc urna. In sollicitudin in lorem ac bibendum. Pellentesque faucibus imperdiet erat a posuere. Curabitur finibus nibh a dignissim sollicitudin. Aliquam euismod sodales nibh, ut tristique lectus vestibulum a. Duis sit amet interdum ipsum, in dapibus leo. Quisque sodales luctus cursus. Phasellus a convallis mauris.</p>
+                </Container>
               </Grid>
 
               <Grid item xs={12} id="contacFooter-storeDetail">
@@ -59,11 +63,11 @@ export default function TiendaDetalle() {
                   <Grid item xs={12} sm={8} md={8}>
                     <h3>¿Como comprar?</h3>
                     <List >
-                      <li>
+                      <ListItem>
                         <Link className="icon-what" href="https://api.whatsapp.com/send?phone=573015407389&amp;text=Hola,%20me%20gustaria%20daber%20si%20tienes%20domicilio.%20Espero%20su%20respuesta,%20gracias." target="_blank"> <WhatsAppIcon />
                           <Typography className="text"><span>WhatsApp:</span> 3015407389</Typography>
                         </Link>
-                      </li>
+                      </ListItem>
                       <ListItem >
                         <ListItemIcon className="icon-phone"> <PhoneIcon /> </ListItemIcon>
                         <Typography className="text"><span>Telefono:</span> 8405083</Typography>
